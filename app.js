e5cab7297fc968ed310d392218e6731d91c90548
Dustin Myers <dustinblaine.myers@gmail.com>
2020-02-26 13:19:47 -0500

    working app

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ app.listen(3333, () => {
 const rootController = require(`./routes/index`),
 catController = require(`./routes/cat`),
 dogController = require(`./routes/dog`),
-fooController = require(`./routes/foo`);
+fooController = require(`./routes/foo`)
+personController = require('./routes/person');
 
 app.use('/', rootController);
 app.use('/foo', fooController);
 app.use(`/cat`, catController);
 app.use(`/dog`, dogController);
-
+app.use('/person', personController);
 
 
